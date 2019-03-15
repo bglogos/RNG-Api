ec7806279cb74c6ac3b5d65ff8612d1076a87b41
Tsvetan Smardanski <tsvetan.smardanski@tfs.negometrix.com>
2019-03-15 11:10:43 +0200

    Initial commit

---
+++ RngApi/Controllers/RngController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RngApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RngController : ControllerBase
+    {
+        [HttpGet("{count}")]
+        public ActionResult<int[]> Get(int count)
+        {
+            Random random = new Random();
+            int[] result = new int[count];
+
+            for (int idx = 0; idx < count; idx++)
+            {
+                result[idx] = random.Next();
+            }
+
+            return result;
+        }
+    }
+}
